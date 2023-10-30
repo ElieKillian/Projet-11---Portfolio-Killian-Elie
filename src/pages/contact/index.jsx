55d419165ffa89a6637966168d0022ae1eb5677f
Killian Elie <killianelie@gmail.com>
2023-10-30 17:31:51 +0100

    maj form

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -16,7 +16,11 @@ function Contact(){
     const Submit = (e) => {
         e.preventDefault();
 
-        // Configuration EmailJS (remplacez les valeurs par les vôtres)
+        if (!formData.nom || !formData.prenom || !formData.email || !formData.message) {
+            alert('Veuillez remplir tous les champs obligatoires.');
+            return;
+        }
+
         const templateParams = {
             nom: formData.nom,
             prenom: formData.prenom, 
@@ -30,7 +34,6 @@ function Contact(){
         emailjs.send('service_ufpg4pv', 'template_h71wxer', templateParams, 'E2U49VR9dmBpuDc5z')
             .then(function (response) {
                 console.log('E-mail envoyé avec succès!', response);
-                // Réinitialisez le formulaire après l'envoi réussi
                 setFormData({
                     nom: '',
                     prenom: '',
@@ -48,14 +51,15 @@ function Contact(){
             <div className="page__card">
                 <div className="page__card__container">
                     <h2>Me contacter</h2>
+                    <p>Champs obligatoires*</p>
                     <form className="page__card__container__form">
                         <div className="page__card__container__form__partname">
                             <div className="page__card__container__form__partname__part">
-                                <label htmlFor="nom" >Nom</label>
+                                <label htmlFor="nom" >Nom*</label>
                                 <input name="nom" value={formData.nom} onChange={(e) => setFormData({ ...formData, nom: e.target.value })} required />
                             </div>
                             <div className="page__card__container__form__partname__part">
-                                <label htmlFor="prenom" >Prénom</label>
+                                <label htmlFor="prenom" >Prénom*</label>
                                 <input name="prenom" value={formData.prenom} onChange={(e) => setFormData({ ...formData, prenom: e.target.value })} required />
                             </div>
                         </div>
@@ -64,11 +68,11 @@ function Contact(){
                             <input name="entreprise" value={formData.entreprise} onChange={(e) => setFormData({ ...formData, entreprise: e.target.value })} />
                         </div>
                         <div className="page__card__container__form__part">
-                            <label htmlFor="email" >Email</label>
+                            <label htmlFor="email" >Email*</label>
                             <input name="email" type="email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} required />
                         </div>
                         <div className="page__card__container__form__part">
-                            <label htmlFor="message" >Message</label>
+                            <label htmlFor="message" >Message*</label>
                             <textarea name="message" rows="5" cols="10" value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })} required />
                         </div>
                         <button className="page__card__container__form__button" onClick={Submit} >Envoyer</button>
